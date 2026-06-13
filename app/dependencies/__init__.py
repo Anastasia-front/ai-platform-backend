08e56feb0c63dc91926cdf39433ecc391757ccb7
Anastasia Prysiazhna <prysiazhnastasia@gmail.com>
2026-06-13 15:59:48 +0200

    Update dependencies reexport

--- app/dependencies/__init__.py
+++ app/dependencies/__init__.py
@@ -1,12 +1,34 @@
 from .auth import get_current_user
 from .chat import get_owned_chat
 from .project import get_owned_project
+from .repositories import (
+    get_chat_repository,
+    get_document_repository,
+    get_message_repository,
+    get_project_repository,
+    get_workflow_event_repository,
+    get_workflow_repository,
+    get_workflow_run_repository,
+    get_workflow_step_run_repository,
+)
 from .workflow import get_owned_workflow, get_workflow_service
+from .workflow_run import get_owned_workflow_run
+from .workflow_steps import get_owned_workflow_step
 
 __all__ = [
+    "get_chat_repository",
     "get_current_user",
-    "get_workflow_service",
+    "get_document_repository",
+    "get_message_repository",
     "get_owned_chat",
     "get_owned_project",
     "get_owned_workflow",
+    "get_owned_workflow_run",
+    "get_owned_workflow_step",
+    "get_project_repository",
+    "get_workflow_event_repository",
+    "get_workflow_repository",
+    "get_workflow_run_repository",
+    "get_workflow_service",
+    "get_workflow_step_run_repository",
 ]
