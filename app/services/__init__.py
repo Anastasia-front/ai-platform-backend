dcb44bc265e9d888ac3f3d859d1edfe9a3722bf0
Anastasia Prysiazhna <prysiazhnastasia@gmail.com>
2026-06-14 13:50:35 +0200

    Update services reexport

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,13 +1,9 @@
 from .ai import AIService
-from .auth import authenticate_user, create_user, get_user_by_email
-from .event_bus import EventBus
+from .auth import AuthService
 from .workflow.workflow import WorkflowService
 
 __all__ = [
     "AIService",
-    "authenticate_user",
-    "create_user",
-    "get_user_by_email",
-    "EventBus",
+    "AuthService",
     "WorkflowService",
 ]
