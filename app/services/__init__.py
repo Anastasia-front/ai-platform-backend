fbbdefb54378f632ac57cb13c7d99b315a2266a5
Anastasia Prysiazhna <prysiazhnastasia@gmail.com>
2026-06-19 14:24:04 +0200

    Add reexport from services

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,9 +1,27 @@
 from .ai import AIService
 from .auth import AuthService
+from .chunk import ChunkService
+from .document import DocumentService
+from .embedding import EmbeddingService
+from .extractors import (
+    DEFAULT_EXTRACTORS,
+    DocxExtractor,
+    PdfExtractor,
+    TextExtractor,
+    TxtExtractor,
+)
 from .workflow.workflow import WorkflowService
 
 __all__ = [
     "AIService",
     "AuthService",
     "WorkflowService",
+    "ChunkService",
+    "DocumentService",
+    "EmbeddingService",
+    "DEFAULT_EXTRACTORS",
+    "DocxExtractor",
+    "PdfExtractor",
+    "TextExtractor",
+    "TxtExtractor",
 ]
