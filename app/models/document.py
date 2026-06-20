8d0594a679e860b14139a60d42235469ae36bf60
Anastasia Prysiazhna <prysiazhnastasia@gmail.com>
2026-06-20 17:39:54 +0200

    Add metrics for document

--- app/models/document.py
+++ app/models/document.py
@@ -1,8 +1,8 @@
-from sqlalchemy import Enum, ForeignKey, Integer, String, Text
+from sqlalchemy import DateTime, Enum, ForeignKey, Integer, String, Text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.core import Base
-from app.enums import DocumentStatus
+from app.enums import DocumentStatus, EmbeddingStatus
 from app.models.mixins import TimestampMixin
 
 
@@ -26,16 +26,28 @@ class Document(TimestampMixin, Base):
     file_size: Mapped[int] = mapped_column(Integer, nullable=False)
 
     status: Mapped[DocumentStatus] = mapped_column(
-        Enum(DocumentStatus, 
-        values_callable=lambda enum: [e.value for e in enum],
-        native_enum=False),
-        default=DocumentStatus.UPLOADED,
+        Enum(
+            DocumentStatus,
+            values_callable=lambda enum: [e.value for e in enum],
+            native_enum=False,
+        ),
+        default=DocumentStatus.UPLOADED.value,
         nullable=False,
     )
 
     text: Mapped[str | None] = mapped_column(
         Text,
         nullable=True,
+    )
+
+    processing_started_at = mapped_column(DateTime(timezone=True), nullable=True)
+    processing_finished_at = mapped_column(DateTime(timezone=True), nullable=True)
+    processing_duration_ms = mapped_column(Integer, nullable=True)
+
+    embedding_status = mapped_column(
+        Enum(EmbeddingStatus),
+        default=EmbeddingStatus.PENDING.value,
+        nullable=False,
     )
 
     project = relationship(
